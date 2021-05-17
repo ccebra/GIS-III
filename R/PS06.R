5b3073459a28ba99c6ccafaa48a82ec7962d058f
Christopher Cebra <ccebra@uchicago.edu>
2021-05-16 23:12:05 -0500

    Added package to Git repository

---
+++ R/PS06.R
@@ -0,0 +1,13 @@
+#' Medicaid Expenditure 2019
+#'
+#' A dataset containing medicaid expenditure data at the state level for 2019.
+#'
+#' @format A data frame with 51 rows and 4 variables:
+#' \describe{
+#' \item{STATEFP}{STATEFP}
+#' \item{TtlMedExpC}{Total medicaid spending in character format}
+#' \item{TtlMedExpN}{Total medicaid spending in numeric format}
+#' \item{Year}{year of the data}
+#' }
+#' @source \url{https://www.kff.org/medicaid/state-indicator/total-medicaid-spending/?}
+"PS06"
